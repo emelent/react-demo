bf4622dc47ed4640528b6ab0e4741826d7c9a755
Roy <roy.motloutsi@gmail.com>
2020-02-16 18:26:36 +0200

    Update App component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Adder from './Adder'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+	state = {
+		contestantData: {}
+	}
+
+	updateContestantData  = (name, points)  => {
+		if(this.state.contestantData.hasOwnProperty(name)){
+			this.setState({
+				contestantData: {
+					...this.state.contestantData,
+					[name]: this.state.contestantData[name] + points
+				}
+			})
+		} else {
+			this.setState({
+				contestantData: {
+					...this.state.contestantData,
+					[name]: points
+				}
+			})
+		}
+	}
+
+	render(){
+		return (
+			<div className="App">
+				<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<Adder onSubmit={this.updateContestantData} />
+				</header>
+			</div>
+			)
+	}
+	
 }
 
-export default App;
+export default App
